6ee097c8ca9df49d2e7d80c59b514837286eab97
Victor <victor.dmaina@gmail.com>
2024-09-26 15:16:27 +0300

    create menu list for footer

---
+++ frontend/src/components/Footer/Menu.tsx
@@ -0,0 +1,44 @@
+type LinkItem = {
+    name: string;
+    link: string;
+  };
+  
+  export const PRODUCTS: LinkItem[] = [
+    { name: "Game Bundles", link: "#" },
+    { name: "DLCs", link: "#" },
+    { name: "Omega Merchandise", link: "#" },
+    { name: "Omega Gift Cards", link: "#" },
+  ];
+  
+  export const RESOURCES: LinkItem[] = [
+    { name: "Game Guides", link: "#" },
+    { name: "Game Demos", link: "#" },
+    { name: "Developer Documentation", link: "#" },
+    { name: "Online events", link: "#" },
+    { name: "System Requirements", link: "#" },
+  ];
+  
+  export const COMPANY: LinkItem[] = [
+    { name: "About Us", link: "#" },
+    { name: "Partnerships", link: "#" },
+    { name: "Team/Staff Profiles", link: "#" },
+    { name: "Privacy Policy", link: "#" },
+    { name: "Terms of Service", link: "#" },
+  ];
+  
+  export const SUPPORT: LinkItem[] = [
+    { name: "FAQs", link: "#" },
+    { name: "Account Management", link: "#" },
+    { name: "Billing and Payments", link: "#" },
+    { name: "Community Guidelines", link: "#" },
+    { name: "Security Information", link: "#" },
+  ];
+  
+  export const Icons: LinkItem[] = [
+    { name: "logo-facebook", link: "#" },
+    { name: "logo-twitter", link: "#" },
+    { name: "logo-github", link: "#" },
+    { name: "logo-linkedin", link: "#" },
+    { name: "logo-instagram", link: "#" },
+  ];
+  
