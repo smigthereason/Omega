2e2633728a8f4be6137278d9664be999f25ea21f
stephy <stephshiro35@gmail.com>
2024-09-30 15:47:11 +0300

    add and convert items and itemscontaier files to tsx format

---
+++ frontend/src/components/Item.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Define the type for each link
+interface Link {
+  name: string;
+  link: string;
+}
+
+// Define the props type for the Item component
+interface ItemProps {
+  Links: Link[];
+  title: string;
+}
+
+const Item: React.FC<ItemProps> = ({ Links, title }) => {
+  return (
+    <ul>
+      <h1 className="mb-1 font-semibold">{title}</h1>
+      {Links.map((link) => (
+        <li key={link.name}>
+          <a
+            className="text-gray-400 hover:text-teal-400 duration-300
+          text-sm cursor-pointer leading-6"
+            href={link.link}
+          >
+            {link.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Item;
