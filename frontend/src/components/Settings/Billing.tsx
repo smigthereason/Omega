d90f5cb9f9e4526004c2ffdee2a0626757739f85
stephy <stephshiro35@gmail.com>
2024-09-30 16:04:22 +0300

    add billing and change pasword file and style them using tailwind

---
+++ frontend/src/components/Settings/Billing.tsx
@@ -0,0 +1,80 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+
+const Billing: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [cardNumber, setCardNumber] = useState<string>('');
+
+  const handleOptionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(e.target.value);
+  };
+
+  const handleCardNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCardNumber(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    console.log({ selectedOption, cardNumber });
+    alert('Billing information submitted successfully!');
+  };
+
+  return (
+    <div className="bg-black rounded-lg p-6 w-full pt-10 justify-center h-screen">
+      <h1 className="font-bold text-lg mb-14 uppercase text-white text-center ">Billing Information</h1>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto justify-center">
+        <div className="mb-4">
+          <label htmlFor="paymentOption" className="block text-sm font-medium  text-white">
+            Select Payment Option
+          </label>
+          <select
+            id="paymentOption"
+            name="paymentOption"
+            value={selectedOption}
+            onChange={handleOptionChange}
+            className="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-s text-black"
+          >
+            <option value="">Select Payment Option</option>
+            <option value="mpesa">Mpesa</option>
+            <option value="paypal">PayPal</option>
+            <option value="mastercard">Mastercard</option>
+            <option value="visa">Visa</option>
+          </select>
+        </div>
+        {selectedOption && (
+          <div className="mb-4">
+            <label htmlFor="cardNumber" className="block text-sm font-medium text-white">
+              Enter Card Number
+            </label>
+            <input
+              type="tel"
+              id="cardNumber"
+              name="cardNumber"
+              value={cardNumber}
+              onChange={handleCardNumberChange}
+              placeholder="Enter card number"
+              className=" text-black mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        )}
+        <button
+          type="submit"
+          className=" text-white px-4 py-2 ml-32 rounded-md transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+        >
+          Submit
+        </button>
+        <Link to="/settings">
+          <button
+            type="button"
+            className="btn-default ml-2 text-white transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+          >
+            Cancel
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Billing;
---
+++ frontend/src/components/Settings/ChangePassword.tsx
@@ -0,0 +1,122 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import { Link } from "react-router-dom";
+import Logo from "./logo-no-background.png";
+import VideoBackground from "./pass.mp4";
+
+interface FormData {
+  currentPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
+
+function ChangePassword() {
+  const [formData, setFormData] = useState<FormData>({
+    currentPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+
+  const [isRegistered, setIsRegistered] = useState<boolean>(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    setIsRegistered(true);
+  };
+
+  if (isRegistered) {
+    window.location.href = "/"; 
+  }
+
+  return (
+    <>
+      <section className="video-section">
+        <video
+          src={VideoBackground}
+          autoPlay
+          loop
+          muted
+          className="video-background"
+        />
+
+        <div className="flex items-center justify-center bg-cover h-screen ">
+          <div className="w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-transparent bg-opacity-60 backdrop-blur-lg shadow-lg rounded-lg overflow-hidden  px-8 pt-6 pb-8 mb-4">
+            <Link
+              to="/settings"
+              className="flex items-center text-white ml-28 px-10 py-8 "
+            >
+              <img
+                src={Logo}
+                alt="Logo"
+                className="h-14 w-auto transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+              />
+            </Link>
+            <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+              <div className="mb-4">
+                <label htmlFor="currentPassword" className="block mb-1">
+                  Current Password
+                </label>
+                <input
+                  type="password"
+                  id="currentPassword"
+                  name="currentPassword"
+                  value={formData.currentPassword}
+                  onChange={handleChange}
+                  className="border p-2 w-full rounded-xl"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="newPassword" className="block mb-1">
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  id="newPassword"
+                  name="newPassword"
+                  value={formData.newPassword}
+                  onChange={handleChange}
+                  className="border p-2 w-full rounded-xl"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="confirmNewPassword" className="block mb-1">
+                  Confirm New Password
+                </label>
+                <input
+                  type="password"
+                  id="confirmNewPassword"
+                  name="confirmNewPassword"
+                  value={formData.confirmNewPassword}
+                  onChange={handleChange}
+                  className="border p-2 w-full rounded-xl"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className=" text-white text-center px-2 py-2 ml-28 transition ease-in-out hover:-translate-y-1 hover:scale-110  duration-300"
+              >
+                Change Password
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ChangePassword;
